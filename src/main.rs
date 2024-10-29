644ee9b5670fa864b6b07663d07513c15339fc79
Emmanuel Onyekwelu <theizaboss@gmail.com>
2024-10-29 03:55:50 -0300

    push

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 #![feature(gen_blocks)]
 #![feature(trait_upcasting)]
-#![feature(unboxed_closures,fn_traits)]
+
 use bevy_reflect::{FromType, GetField, Reflect, TypeData};
-#[allow(static_mut_refs)]
+
 use std::any::Any;
 use std::fmt::Display;
 use std::ops::Deref;
