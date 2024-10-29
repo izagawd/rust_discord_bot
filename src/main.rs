981be8735e4a7a53cb29eb0ef1cce4da3376a969
Emmanuel Onyekwelu <theizaboss@gmail.com>
2024-10-29 00:16:14 -0300

    push

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,67 @@
 #![feature(gen_blocks)]
 #![feature(trait_upcasting)]
 #![feature(unboxed_closures,fn_traits)]
-
+#![feature(specialization)]
+#[allow(static_mut_refs)]
+use std::any::{Any, TypeId};
+use std::fmt::Display;
 use std::mem;
+use std::ops::Deref;
+use std::rc::Rc;
+use bevy_reflect::{reflect_trait, FromType, Reflect, TypeData, TypeRegistry};
+use tokio::task::id;
+use tokio::time::interval;
+use crate::useful_funcs::{get_reflect_type, register_type};
 
 mod bot;
 mod help;
 mod ping;
 mod rps;
+mod useful_funcs;
+
+#[derive(Reflect)]
+#[reflect(MyTrait)]
+#[reflect(MyOtherTrait)]
+struct MyType {
+    value: String,
+}
 
 
+impl MyTrait for MyType {
+    fn do_thing(&self) -> String {
+        format!("{} World!", self.value)
+    }
+}
+
+
+#[reflect_trait]
+pub trait MyTrait {
+    fn do_thing(&self) -> String;
+}
+#[reflect_trait]
+pub trait MyOtherTrait {
+    fn do_thing(&self) -> String;
+}
+impl MyOtherTrait for MyType {
+    fn do_thing(&self) -> String {
+        String::from("dd")
+    }
+}
 #[tokio::main]
 async fn main() {
+    let idk : &dyn Reflect = &MyType{ value: "".to_string() };
+    register_type::<MyType>();
+    let gotten = get_reflect_type::<ReflectMyOtherTrait>(idk);
 
-    bot::start().await;
+    if(gotten.is_ok()){
+       let unwrapped = gotten.unwrap();
+       let casted = unwrapped.get(idk);
+        println!("{}", casted.unwrap().do_thing());
+    } else{
+        println!("aww");
+    }
+
 }
+// This works because the #[reflect(MyTrait)] we put on MyType informed the Reflect derive to insert a new instance
+// of ReflectDoThing into MyType's registration. The instance knows how to cast &dyn Reflect to &dyn DoThing, because it
+// knows that &dyn Reflect should first be downcasted to &MyType, which can then be safely casted to &dyn DoThing
---
+++ src/useful_funcs.rs
@@ -0,0 +1,37 @@
+use std::any::Any;
+use std::ops::Deref;
+use std::sync::{LazyLock, Mutex, RwLock};
+use bevy_reflect::{Reflect, TypeRegistry};
+
+
+pub static REGISTRAR
+: LazyLock<RwLock<TypeRegistry>> = LazyLock::new(||{
+
+
+    RwLock::new(TypeRegistry::default())
+});
+pub fn get_reflect_type<TReflectType: std::marker::Send + std::marker::Sync + std::clone::Clone + 'static>(to_reflect: &dyn Reflect) -> Result<TReflectType, String>{
+
+    let bru =REGISTRAR.read();
+    if(bru.is_ok()){
+        let data = bru.as_ref().unwrap().get_type_data::<TReflectType>((to_reflect as &dyn Any).type_id());
+        if data.is_some(){
+            return Ok(data.unwrap().clone());
+        }else{
+            return Err(String::from("Inputted type not registered"))
+        }
+    } else{
+        return Err(String::from("Another thread has caused an error"));
+    }
+
+}
+pub fn register_type<T: Sized + bevy_reflect::GetTypeRegistration>(){
+    let the_register = REGISTRAR.write();
+    if(the_register.is_ok()){
+
+        the_register.unwrap().register::<T>();
+
+    } else{
+        panic!("This thread has caused an error")
+    }
+}
