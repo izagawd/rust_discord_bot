8e24d458f115f14201e4ed02aee80a7d337a1b4e
Emmanuel Onyekwelu <theizaboss@gmail.com>
2024-11-04 20:05:05 -0400

    push

--- src/useful_funcs.rs
+++ src/useful_funcs.rs
@@ -9,13 +9,14 @@ pub static REGISTRAR
 
     RwLock::new(TypeRegistry::default())
 });
-pub fn get_reflect_type<TReflectType: std::marker::Send + std::marker::Sync + std::clone::Clone + 'static>(to_reflect: &dyn Reflect) -> Result<TReflectType, String>{
+pub fn get_reflect_type<TReflectType: std::marker::Send + std::marker::Sync + std::clone::Clone
++ 'static>(to_reflect: &dyn Reflect) -> Result<TReflectType, String>{
 
-    let bru =REGISTRAR.read();
-    if bru.is_ok(){
-        let data = bru.as_ref().unwrap().get_type_data::<TReflectType>((to_reflect as &dyn Any).type_id());
-        if data.is_some(){
-            return Ok(data.unwrap().clone());
+
+    if let Ok(ref bru) = REGISTRAR.read(){
+        let data = bru.get_type_data::<TReflectType>((to_reflect as &dyn Any).type_id());
+        if let Some(ref to_reflect) = data{
+            return Ok((*to_reflect).clone());
         }else{
             return Err(String::from("Inputted trait not registered for inputted object"))
         }
@@ -25,12 +26,10 @@ pub fn get_reflect_type<TReflectType: std::marker::Send + std::marker::Sync + st
 
 }
 pub fn register_type<T: Sized + bevy_reflect::GetTypeRegistration>(){
-    let the_register = REGISTRAR.write();
-    if the_register.is_ok(){
-
-        the_register.unwrap().register::<T>();
-
+    if let Ok(ref mut the_register) = REGISTRAR.write(){
+        the_register.register::<T>()
     } else{
         panic!("This thread has caused an error. this is not supposed to happen")
     }
+
 }
